47202d2a1187a57bec4e25245a46364597aa0bdc
张飞 <zhangfei@zhangfeideMacBook-Pro.local>
2022-03-22 09:36:57 +0800

    feat:给选择排序添加了注释

--- 算法&数据结构/算法/排序/选择排序.js
+++ 算法&数据结构/算法/排序/选择排序.js
@@ -1,11 +1,13 @@
 Array.prototype.selectionSort = function selectionSort() {
   for (let i = 0; i < this.length - 1; i++) {
     let minIndex = i
+    // 先查找最小元素
     for (let j = i; j < this.length; j++) {
       if (this[j] < this[minIndex]) {
         minIndex = j
       }
     }
+    // 找到之后进行和当前元素进行替换
     if(this[i] !== this[minIndex]){
       let temp = this[i]
       this[i] = this[minIndex]
